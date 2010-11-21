c8cc6f48120d0b0cf1c90c7c0bf7a316103f4a9a
searler <eggsearler@verizon.net>
2010-11-20 22:33:30 -0600

    fork kind of working

--- src/test/scala/workflow/Flows.scala
+++ src/test/scala/workflow/Flows.scala
@@ -11,16 +11,16 @@ object SingleLineBalance{
 
 object TwoLineBalance{
     def apply(pn:Num)(implicit acctLook:Lookup[Num,Acct],  balLook:Lookup[Acct,Bal]) = {
-       var total = Bal(0)
+       
       // acctLook(pn){a:Acct => balLook(a){b:Bal=> total= total+ b.v; Bal(total)}}
-       val  next = {b:Bal => total}
-       concat(next)({c => acctLook(pn){a:Acct => balLook(a){b:Bal => total = total + b;c(total)}}} , {c=>acctLook(pn){a:Acct => balLook(a){b:Bal => total = total + b;c(total)}}})
+       val  next = {var total = Bal(0);b:Bal => total += b;total}
+       concat(next)({c => acctLook(pn){a:Acct => balLook(a){c}}} , {c=>acctLook(pn){a:Acct => balLook(a){c}}})
     }
 
    //fa(0)(f) orElse fa(1)(f) orElse Map()
    def concat[A,B,C,D](f:C=>D)(fa:(C=>Option[D])=>PartialFunction[A,B]*) = {
      var count = fa size
-     def counter(arg:C):Option[D] = {count-=1;if(count==0)Some(f(arg)); else None}
+     def counter(arg:C):Option[D] = {count-=1;if(count==0)Some(f(arg));  else {f(arg);None}}
      var s =  (fa.head){counter}
      for(x<-fa.tail) s = s.orElse(x(counter))
      s    
