0dd0a09f7f45102bec2d841652ef8e2f508f5d52
searler <eggsearler@verizon.net>
2010-11-20 22:28:29 -0600

    fork kind of working

--- src/test/scala/workflow/Flows.scala
+++ src/test/scala/workflow/Flows.scala
@@ -13,15 +13,19 @@ object TwoLineBalance{
     def apply(pn:Num)(implicit acctLook:Lookup[Num,Acct],  balLook:Lookup[Acct,Bal]) = {
        var total = Bal(0)
       // acctLook(pn){a:Acct => balLook(a){b:Bal=> total= total+ b.v; Bal(total)}}
-       val  common = {b:Bal => total = total + b;total}
-       concat(common)({c => acctLook(pn){a:Acct => balLook(a)(c)}} , {c=>acctLook(pn){a:Acct => balLook(a)(c)}})
+       val  next = {b:Bal => total}
+       concat(next)({c => acctLook(pn){a:Acct => balLook(a){b:Bal => total = total + b;c(total)}}} , {c=>acctLook(pn){a:Acct => balLook(a){b:Bal => total = total + b;c(total)}}})
     }
 
    //fa(0)(f) orElse fa(1)(f) orElse Map()
-   def concat[A,B,C,D](f:C=>D)(fa:(C=>D)=>PartialFunction[A,B]*) = {
-     var s =  (fa.head)(f)
-     for(x<-fa.tail) s = s.orElse(x(f))
+   def concat[A,B,C,D](f:C=>D)(fa:(C=>Option[D])=>PartialFunction[A,B]*) = {
+     var count = fa size
+     def counter(arg:C):Option[D] = {count-=1;if(count==0)Some(f(arg)); else None}
+     var s =  (fa.head){counter}
+     for(x<-fa.tail) s = s.orElse(x(counter))
      s    
+
+     
     
 }
    
--- src/test/scala/workflow/FlowsTest.scala
+++ src/test/scala/workflow/FlowsTest.scala
@@ -20,13 +20,15 @@ Services.requests
 Services.requests
    val cb1 = TwoLineBalance(Num("124-555-1234"))
    val cb2 = cb1(1)(Acct("alpha"))
-   val bal1:Bal = cb2(3)(Bal(124.5F))
    val cb3 = cb1(2)(Acct("alpha"))
-   val bal2:Bal = cb3(4)(Bal(124.5F))
-   Bal(124.5F) must beEqualTo(bal1)
-   Bal(249.0F) must beEqualTo(bal2)
+   val bal1:Option[Bal] = cb2(3)(Bal(124.5F))
+   val bal2:Option[Bal]  = cb3(4)(Bal(124.5F))
+   None must beEqualTo(bal1)
+   Some(Bal(249.0F)) must beEqualTo(bal2)
   List(Num("124-555-1234"),Num("124-555-1234"),Acct("alpha"),Acct("alpha")) must beEqualTo(Services.requests)
   } 
+
+
 
 
  
