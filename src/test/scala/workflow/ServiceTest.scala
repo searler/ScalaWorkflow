bcc5467a5a6ed78d0fde77afbee5cb3102125818
searler <eggsearler@verizon.net>
2010-12-25 12:02:27 -0600

    tidy tests

--- src/test/scala/workflow/ServiceTest.scala
+++ src/test/scala/workflow/ServiceTest.scala
@@ -6,28 +6,21 @@ object ServicesTest extends Specification{
 
 
  import Services._
+import Flow._
 
 
 object Extract{
    def apply[A](r:RPF):A = (r.asInstanceOf[Result[A]]).value
 }
 
-/*
+
  "account lookup" in {
-    Services.requests
-    val cb = AccountLookup(Num("123-555-1234"))(End)
+    val cb = acctLook(Num("124-555-1234"))(End)
     
     val a = Acct("alpha")
-    val res = cb(CI("1"))(a)
+    val res = cb(CI(1))(a)
     val r:Acct = Extract(res)
     a must beEqualTo(r)
-
-   val ra:Any = Extract(res)
-    a must beEqualTo(ra)
-
-   
-
-   List(Num("123-555-1234")) must beEqualTo(requests)
  } 
-*/
+
 }
