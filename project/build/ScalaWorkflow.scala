e7c194bc2673108cc4058e1af1787911b2aa9d8f
Richard Searle <harold.richard.searle@gmail.com>
2011-02-26 11:48:20 -0600

    update dependency version

--- project/build/ScalaWorkflow.scala
+++ project/build/ScalaWorkflow.scala
@@ -5,12 +5,11 @@ import reaktor.scct.ScctProject
 
 class ScalaWorkflowProject(info: ProjectInfo) extends DefaultProject(info)  with AkkaProject with ScctProject
 {
-    val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6"
+    val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7"
 
-    val akkaRep = " Akka repository" at "http://www.scalablesolutions.se/akka/repository/"
-
+   
     val akkaCamel = akkaModule("camel")
-    val camelCore = "org.apache.camel" % "camel-core" % "2.5.0"
-    val camelScala = "org.apache.camel" % "camel-scala" % "2.5.0"
+    val camelCore = "org.apache.camel" % "camel-core" % "2.6.0"
+    val camelScala = "org.apache.camel" % "camel-scala" % "2.6.0"
 
 }
