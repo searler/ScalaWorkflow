754024d8e1f64645c55db5565d220c3238732fbd
Richard Searle <harold.richard.searle@gmail.com>
2011-02-26 11:48:35 -0600

    update dependency version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,8 +1,8 @@
 import sbt._
  
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val akkaRep = " Akka repository" at "http://www.scalablesolutions.se/akka/repository/"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0-RC1"
+  val akkaRep = " Akka repository" at "http://akka.io/repository"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0"
   val scctRepo = "scct-repo" at "http://mtkopone.github.com/scct/maven-repo/"
   lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
 
