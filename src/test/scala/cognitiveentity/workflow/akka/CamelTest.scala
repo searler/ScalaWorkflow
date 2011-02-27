88d73edf301f8489a911b880a04a0c1996b97356
Richard Searle <harold.richard.searle@gmail.com>
2011-02-27 15:59:28 -0600

    comments

--- src/test/scala/cognitiveentity/workflow/akka/CamelTest.scala
+++ src/test/scala/cognitiveentity/workflow/akka/CamelTest.scala
@@ -24,7 +24,7 @@ import akka.camel.CamelContextManager
 import akka.camel.CamelServiceManager._
 
 /**
- * Simulates external services that provide
+ * Simulates external services that provides
  *  id             -> corresponding phone numbers
  *  phone number   -> account number
  *  account number -> balance
@@ -44,7 +44,9 @@ private object Responder{
 }
 
 
- 
+ /**
+  * Implement lookup services using a seda end points
+  */
 private object Service{
   private class Service(name:String) extends akka.actor.Actor with akka.camel.Producer{
      def endpointUri = "seda:" + name
@@ -56,8 +58,15 @@ private object Service{
   val ppService = akka.actor.Actor.actorOf(new Service("pp")).start
 }
 
+/**
+ * Assemble:
+ * - Camel
+ * - FlowsSwitch
+ * - above services
+ */
 private trait Wiring extends AkkaFlowActor {  
  
+  //Return the flow that handles a
   override def create(a:Any) =  cs(a)
   
   import Service._
@@ -69,44 +78,58 @@ private trait Wiring extends AkkaFlowActor {
 
   val cs = new cognitiveentity.workflow.FlowsSwitch
 
+  // unwrap contents of Camel Message
   override def receive = {
       case akka.camel.Message(a,_) => super.receive(a)
       case _ @ x => super.receive(x)
   }
 }
 
+/**
+ * Perform R/R MEP against flow defined in Wiring
+ */
 private class RRLauncher extends RequestResponseAkkaFlowActor with Wiring
-
 private object RRLauncher {
    import cognitiveentity.workflow.Trigger
    def apply[A](initial:A) = {
-     val actRef = akka.actor.Actor.actorOf[RRLauncher]
+     val actRef = akka.actor.Actor.actorOf[RRLauncher] //nust occur here
      actRef !! Trigger(initial)
    }
 } 
 
+/**
+ * Implements a Camel end point to asynchronously catch the results
+ * of a oneway interaction
+ */
 private object RespondTo {
   import akka.actor.Actor
   import akka.actor.Actor._
   import akka.camel.Producer
  
-  class RespondTo extends Actor with Producer {
+  private class RespondTo extends Actor with Producer {
       def endpointUri = "seda:gather"
   }
 
    val dest = actorOf[RespondTo].start
 }
 
+/**
+ * Perform oneway MEP against flow defined in Wiring,
+ * with ultimate result delivered asynchronously to RespondTo
+ */
 private class SendLauncher(respondTo:akka.actor.ActorRef) extends FixedResponseAkkaFlowActor(respondTo) with Wiring
-
 private object SendLauncher {
    import cognitiveentity.workflow.Trigger
    def apply[A](initial:A)  {
-     val actRef = akka.actor.Actor.actorOf(new SendLauncher(RespondTo.dest))
+     val actRef = akka.actor.Actor.actorOf(new SendLauncher(RespondTo.dest)) //must occur here
      actRef ! Trigger(initial)
    }
 } 
 
+/**
+ * Mechanism to asynchronously capture the results of a one-way
+ * interaction.
+ */
 private object Gather {
     import java.util.concurrent._
     import java.util.concurrent.atomic._
@@ -114,13 +137,16 @@ private object Gather {
     val awaiter = new AtomicReference[CountDownLatch]
     val values = new ListBuffer[Any]
 
+    //Reset the state and indicate the number of expected results
     def prep(expected:Int){
         synchronized {
            values.clear
            awaiter.set(new CountDownLatch(expected))
          }
     }
+    //wait for expected number of results to be received
     def await{awaiter.get.await(2,java.util.concurrent.TimeUnit.SECONDS)}
+    //record the result
     def apply[A](arg:A) {
         synchronized {
           values+=arg
@@ -139,26 +165,34 @@ object CamelTest extends org.specs.Specification {
      val context = CamelContextManager.mandatoryContext
 
      import org.apache.camel.scala.dsl.builder.RouteBuilder;
+     //wire end points for services to a single bean
      context.addRoutes(new RouteBuilder{"seda:num".bean(Responder)}) 
      context.addRoutes(new RouteBuilder{"seda:acct".bean(Responder)}) 
      context.addRoutes(new RouteBuilder{"seda:bal".bean(Responder)}) 
      context.addRoutes(new RouteBuilder{"seda:pp".bean(Responder)}) 
+     //wire end point for R-R call of flow
      context.addRoutes(new RouteBuilder{"seda:request".bean(RRLauncher)})
+     //wire end point for fire and forget call of flow
      context.addRoutes(new RouteBuilder{"seda:send".bean(SendLauncher)})
+     //wire end point for result of f-a-f call 
      context.addRoutes(new RouteBuilder{"seda:gather".bean(Gather)})
 
      startCamelService
    }
 
+   /**
+    * Perform R/R MEP via Camel seda
+   */
    def request[A](a:A) = CamelContextManager.mandatoryContext.createProducerTemplate.requestBody("seda:request",a)
+   /**
+    * Perform a f-a-f MEP via Camel seda
+   */
    def send[A](a:A) = CamelContextManager.mandatoryContext.createProducerTemplate.sendBody("seda:send",a)
-
 
     /**
     * Multiple concurrent fire-and-forget invocations to return total balance
     * for an id.
     */
-  
    "sumBalancesSendLots" in  {
      val cnt = 200
      Gather.prep(cnt)
@@ -166,21 +200,15 @@ object CamelTest extends org.specs.Specification {
          akka.actor.Actor.spawn{send(cognitiveentity.workflow.SumBalances(123))}
      Gather.await
      cnt must beEqualTo(Gather.get.length) 
-     Gather.get.foreach { Bal(125.5F) must beEqualTo(_)    }
+     Gather.get.foreach { Bal(125.5F) must beEqualTo(_) }
     }
     
-  
- 
-    
-
-
     /**
      * A single concurrent fire-and-forget invocation
      */
    "numSendOne" in  {
      Gather.prep(1)
-     for(i<-0 until 1)
-         akka.actor.Actor.spawn{send(123)}
+     akka.actor.Actor.spawn{send(123)}
      Gather.await
      List(List(Num("124-555-1234"), Num("333-555-1234"))) must beEqualTo(Gather.get) 
     }
@@ -199,11 +227,16 @@ object CamelTest extends org.specs.Specification {
      Gather.get.foreach { List(Num("124-555-1234"), Num("333-555-1234")) must beEqualTo(_)    }
     }
 
+    /**
+     * Determine phone numbers from id
+     */
     "num" in  {
      Some(List(Num("124-555-1234"),Num("333-555-1234")))  must beEqualTo(request(123))
     }
 
-    
+    /**
+     * Determine balance from phone number
+     */
     "bal" in  {
      Some(Bal(124.5F))  must beEqualTo(request(Num("124-555-1234")))
     }
@@ -235,22 +268,18 @@ object CamelTest extends org.specs.Specification {
      import akka.actor.Actor._
      val template = CamelContextManager.mandatoryContext.createProducerTemplate
      val cnt  = 15 //fails on greater than pool size
-     var success = new java.util.concurrent.atomic.AtomicInteger
+     val success = new java.util.concurrent.atomic.AtomicInteger
      val latch  = new java.util.concurrent.CountDownLatch(cnt)
      for(i<-0 until cnt)
        spawn{ 
            val result= template.requestBody("seda:request",Num("124-555-1234"));
            if(Some(Bal(124.5F)) == result)success.incrementAndGet
-           latch countDown 
+           latch.countDown 
        }
      latch.await
-     cnt must beEqualTo( success.get) 
+     cnt must beEqualTo(success.get) 
    }
 
-
-
-   
-   
     /**
      * Shutdown
      */ 
